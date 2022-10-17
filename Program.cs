d671bee31b7b9300514df1d71a6e0cd6ba25300d
Elix Froylán Castillo Durán <elix.cd@gmail.com>
2022-10-17 11:39:51 -0500

    Se cambio el nombre de la tierra y agrego marte

--- Program.cs
+++ Program.cs
@@ -6,13 +6,13 @@ List<Planet> solarSistem = new List<Planet>();
 
 solarSistem.Add(new Planet(){ Id = 1, Name = "Mercury" });
 solarSistem.Add(new Planet(){ Id = 2, Name = "Venus" });
-solarSistem.Add(new Planet(){ Id = 3, Name = "Earth" });
+solarSistem.Add(new Planet(){ Id = 3, Name = "Blue Planet" });
 
+solarSistem.Add(new Planet(){ Id = 3, Name = "Mars" });
 
 solarSistem.ForEach(planet => 
     Console.WriteLine("Hello, {0}!", planet.Name)
 );
 
 Console.WriteLine("Hello, New World!", solarSistem[2].Name);
-
 
