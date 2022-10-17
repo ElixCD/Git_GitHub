226aba9abccfd4887b094e4afa905e387d8caca0
Elix Froylán Castillo Durán <elix.cd@gmail.com>
2022-10-17 12:32:14 -0500

    Se agregaron datos sobre las lunas

--- Program.cs
+++ Program.cs
@@ -6,14 +6,18 @@ Console.WriteLine("Rama Moons");
 
 List<Planet> solarSistem = new List<Planet>();
 
-solarSistem.Add(new Planet(){ Id = 1, Name = "Mercury" });
+solarSistem.Add(new Planet(){ Id = 1, Name = "Mercury", NumberMoons = 0 });
 solarSistem.Add(new Planet(){ Id = 2, Name = "Venus" });
 solarSistem.Add(new Planet(){ Id = 3, Name = "Blue Planet" });
 
 solarSistem.Add(new Planet(){ Id = 4, Name = "Mars" });
 
-solarSistem.ForEach(planet => 
-    Console.WriteLine("Hello, {0}!", planet.Name)
+solarSistem.ForEach(planet => {
+    Console.WriteLine("Hello, {0}!", planet.Name);
+    Console.WriteLine("{0} have {1} moons!", planet.Name, planet.NumberMoons);
+}
+   
+
 );
 
 Console.WriteLine("Hello, New World!", solarSistem[2].Name);
