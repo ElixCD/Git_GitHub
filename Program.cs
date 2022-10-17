3a49db3dd2b4c7f1bc6d8703104bd34be742a38b
Elix Froylán Castillo Durán <elix.cd@gmail.com>
2022-10-17 12:12:49 -0500

    Acctualizo posicion mars

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ solarSistem.Add(new Planet(){ Id = 1, Name = "Mercury" });
 solarSistem.Add(new Planet(){ Id = 2, Name = "Venus" });
 solarSistem.Add(new Planet(){ Id = 3, Name = "Blue Planet" });
 
-solarSistem.Add(new Planet(){ Id = 3, Name = "Mars" });
+solarSistem.Add(new Planet(){ Id = 4, Name = "Mars" });
 
 solarSistem.ForEach(planet => 
     Console.WriteLine("Hello, {0}!", planet.Name)
