9b2d5ceefd69127e9b81ce698253274b71ebae67
Elix Froylán Castillo Durán <elix.cd@gmail.com>
2022-10-20 10:41:23 -0500

    Se agregaron lunas a blue planet

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ List<Planet> solarSistem = new List<Planet>();
 
 solarSistem.Add(new Planet(){ Id = 1, Name = "Mercury", NumberMoons = 0 });
 solarSistem.Add(new Planet(){ Id = 2, Name = "Venus" });
-solarSistem.Add(new Planet(){ Id = 3, Name = "Blue Planet" });
+solarSistem.Add(new Planet(){ Id = 3, Name = "Blue Planet" , NumberMoons = 1  });
 
 solarSistem.Add(new Planet(){ Id = 4, Name = "Mars" });
 
